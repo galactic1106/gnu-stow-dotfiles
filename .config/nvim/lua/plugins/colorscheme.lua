e8b33978d737b08270d52f217703888d2a707ec0
galactic1106 <aleksander.sardara@gmail.com>
2024-12-31 21:24:41 +0100

    yet other minor changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             local bg = "#011628"
+            -- local bg = "none"
             local bg_dark = "#011423"
             local bg_highlight = "#143652"
             local bg_search = "#0A64AC"
