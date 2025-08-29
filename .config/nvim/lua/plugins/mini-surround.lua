2d107c50702536df93ed5cc3cb0fe2a31a63996b
Galactic1106 <aleksander.sardara@gmail.com>
2025-08-29 22:31:52 +0200

    modified the nvim config, AGAIN

---
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    enabled = false,
+    opts = {
+      mappings = {
+        -- add = "sa", -- Add surrounding in Normal and Visual modes
+        -- delete = "sd", -- Delete surrounding
+        -- find = "sf", -- Find surrounding (to the right)
+        -- find_left = "sF", -- Find surrounding (to the left)
+        -- highlight = "sh", -- Highlight surrounding
+        -- replace = "sc", -- Replace surrounding
+        -- update_n_lines = "sn", -- Update `n_lines`
+      },
+    },
+  },
+}
