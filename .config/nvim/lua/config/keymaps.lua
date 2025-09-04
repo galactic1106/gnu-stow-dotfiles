a1c80172a4b52492c2dcd83eb15d740c6383a3a1
Galactic1106 <aleksander.sardara@gmail.com>
2025-09-04 16:07:10 +0200

    updated alacritty,tmux and nvim configs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,11 @@
 -- use `vim.keymap.set`
 
 vim.keymap.set("i", "jj", "<esc>", { desc = "Exit insert mode" })
+
+-- vim-tmux-navigator keymaps
+vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>")
+vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>")
+-- vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>")
+-- vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNext<CR>")
