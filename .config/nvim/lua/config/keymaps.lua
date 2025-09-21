ec4d5a221096dde9993827f0399fb96df4e4cb83
Galactic1106 <aleksander.sardara@gmail.com>
2025-09-21 15:41:51 +0200

    add wezterm config, updated nvim and yazi config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,9 +7,17 @@ end)
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
-vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>fh', ':Pick help<CR>')
+-- vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
+-- vim.keymap.set('n', '<leader>fh', ':Pick help<CR>')
 
-vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fh', ':Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fg', ':Telescope help_tags<cr>')
 
-vim.keymap.set('i','jj', '<esc>')
+-- vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+
+vim.keymap.set("n", "<leader>e", function()
+    require("yazi").yazi()
+end)
+
+vim.keymap.set('i', 'jj', '<esc>')
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,8 +1,17 @@
 require("plugins.tokyonight")
 require("plugins.mini-icons")
-require("plugins.oil-nvim")
-require("plugins.oil-git-nvim")
-require("plugins.oil-lsp-diagnostics-nvim")
-require("plugins.mini-pick")
+
+-- use oil or yazi
+-- require("plugins.oil-nvim")
+-- require("plugins.oil-git-nvim")
+-- require("plugins.oil-lsp-diagnostics-nvim")
+
+-- require("plugins.mini-pick")
+
 require("plugins.nvim-lspconfig")
 require("plugins.mason-nvim")
+
+
+require("plugins.plenary-nvim")
+require("plugins.telescope-nvim")
+require("plugins.yazi-nvim")
