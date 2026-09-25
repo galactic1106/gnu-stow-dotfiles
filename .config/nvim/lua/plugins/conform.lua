5a42c1c286d9233db737b93f1ee92d0861116345
Galactic1106 <aleksander.sardara@gmail.com>
2026-09-26 01:35:43 +0200

    added prettier to conform plugin

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ vim.pack.add({ { src = "https://github.com/stevearc/conform.nvim.git", name = "c
 require("conform").setup({
   formatters_by_ft = {
     gdscript = { "gdformat" },
+    javascript = { "prettier" },
     python = { "ruff_format", "ruff_organize_imports" },
   },
 })
