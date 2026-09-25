1c0a397aad34012525e470211de3c3eaa3e5f327
Galactic1106 <aleksander.sardara@gmail.com>
2026-09-25 16:47:26 +0200

    Substituted tabout with neotab

---
+++ .config/nvim/lua/plugins/neotab.lua
@@ -0,0 +1,30 @@
+vim.pack.add({ { src = "https://github.com/kawre/neotab.nvim.git", name = "neotab" } })
+
+
+require("neotab").setup({
+  tabkey = "<Tab>",
+  reverse_key = "<S-Tab>",
+  act_as_tab = true,
+  behavior = "nested", ---@type ntab.behavior
+  pairs = { ---@type ntab.pair[]
+    { open = "(", close = ")" },
+    { open = "[", close = "]" },
+    { open = "{", close = "}" },
+    { open = "'", close = "'" },
+    { open = '"', close = '"' },
+    { open = "`", close = "`" },
+    { open = "<", close = ">" },
+  },
+  exclude = {},
+  smart_punctuators = {
+    enabled = false,
+    semicolon = {
+      enabled = false,
+      ft = { "cs", "c", "cpp", "java" },
+    },
+    escape = {
+      enabled = false,
+      triggers = {}, ---@type table<string, ntab.trigger>
+    },
+  },
+})
